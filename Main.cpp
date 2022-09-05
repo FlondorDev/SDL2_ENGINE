d13e50224806d9a8d3c636fa616b8e38222a21e2
cons.domenico.landri <cons.domenico.landricina@cy4gate.com>
2022-09-05 12:28:37 +0200

    only engine files

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,7 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
-#include "Engine/include/Engine.hpp"
+#include "include/Engine.hpp"
 #include <iostream>
 
 int main(int argc, char* args[])
@@ -9,7 +9,7 @@ int main(int argc, char* args[])
     SDL_Event e{};
     bool quit = false;
     GFXManager::Init("Chironi");
-    GFXManager::DefaultFont = TTF_OpenFont( "Assets/Roboto-Black.ttf", 28 );
+    GFXManager::DefaultFont = TTF_OpenFont( "Assets/Roboto-Black.ttf", 72 );
 
     GFXManager::Add("Hello", "Assets/previewd.png");
     GFXManager::Add("Chiron", "Assets/Chironi.bmp");
@@ -20,15 +20,17 @@ int main(int argc, char* args[])
     Camera camera {"Main"};
     Camera camera2 {"Gui"};
 
-    camera2.CameraRender.x = 150;
-    camera2.CameraRender.y = 150;
+    camera2.CameraRender.x = 0;
+    camera2.CameraRender.y = 0;
     
     Player player {"Chiron", 150,150, Vector2{30, 30}, 6};
     GameObject obj {"Hello", 1280,100, Vector2{0, 620}};
     GameObject obj2 {"Hello", 150,150, Vector2{0, 470}};
     GameObject obj3 {"Hello", 150,150, Vector2{150, 320}};
+
     GameObject obj4 {"testo", 0, 0, Vector2{0, 0}};
     obj4.rb.DestroyCollider();
+
     CameraManager::Get("Gui")->SetTarget(&obj4);
 
     std::string Title;
